857b369ca46d5c2fc04c1f12ce750bda1ea70f41
Michał Reszka <64952841+reszka-m@users.noreply.github.com>
2022-06-19 17:20:32 +0200

    Saving as PDF

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,150 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Filesystem, Directory, FilesystemDirectory } from '@capacitor/filesystem';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+import { Platform } from '@ionic/angular';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  myForm: FormGroup;
+  pdfObj = null;
+  photoPreview = null;
+  logoData = null;
 
-  constructor() {}
+  constructor(
+    private fb: FormBuilder,
+    private plt: Platform,
+    private http: HttpClient,
+    private fileOpener: FileOpener) {}
+
+  ngOnInit() {
+      this.myForm = this.fb.group({
+        showLogo: true,
+        from: 'Simon',
+        to: 'Max',
+        text: 'TEST'
+      });
+      this.loadLocalAssetToBase64();
+  }
+
+  loadLocalAssetToBase64() {
+    this.http.get('./assets/testImage1.png', { responseType: 'blob' }).subscribe(res => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        this.logoData = reader.result;
+      }
+      reader.readAsDataURL(res);
+    });
+  }
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Camera
+    });
+    console.log('image');
+
+    this.photoPreview = `data:image/jpeg;base64,${image.base64String}`;
+  }
+
+  createPdf() {
+    const formValue = this.myForm.value;
+    const image = this.photoPreview ? {image: this.photoPreview, width: 300} : {};
+
+    let logo = {};
+    if (formValue.showLogo) {
+      logo = { image: this.logoData, width: 50 };
+    }
+
+    const docDefinition = {
+      watermark: { text: 'PDF-Scanner', color: 'blue', opacity: 0.2, bold: true },
+      content: [
+        {
+          columns: [
+            logo,
+            {
+              text: new Date().toTimeString(),
+              alignment: 'right'
+            }
+          ]
+        },
+        { text: 'REMINDER', style: 'header' },
+        {
+          columns: [
+            {
+              width: '50%',
+              text: 'From',
+              style: 'subheader'
+            },
+            {
+              width: '50%',
+              text: 'To',
+              style: 'subheader'
+            }
+          ]
+        },
+        {
+          columns: [
+            {
+              width: '50%',
+              text: formValue.from
+            },
+            {
+              width: '50%',
+              text: formValue.to
+            }
+          ]
+        },
+        image,
+        { text: formValue.text, margin: [0, 20, 0, 0] },
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 15, 0, 0]
+        },
+        subheader: {
+          fontSize: 14,
+          bold: true,
+          margin: [0, 15, 0, 0]
+        }
+      }
+    }
+    this.pdfObj = pdfMake.createPdf(docDefinition);
+  }
+
+  downloadPdf() {
+    if (this.plt.is('cordova')) {
+      this.pdfObj.getBase64(async (data) => {
+        try {
+          let path = `pdf/myletter_${Date.now()}.pdf`;
+
+          const result = await Filesystem.writeFile({
+            path,
+            data,
+            directory: FilesystemDirectory.Documents,
+            recursive: true
+          });
+          this.fileOpener.open(`${result.uri}`, 'application/pdf');
+        } catch (e) {
+          console.error('Unable to write file', e);
+        }
+      });
+    } else {
+      this.pdfObj.download();
+    }
+  }
 
 }
