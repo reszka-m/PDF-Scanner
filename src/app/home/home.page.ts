609bc0aebd53f781f629405f44d03e4eef9f3bf8
Michał Reszka <64952841+reszka-m@users.noreply.github.com>
2022-06-20 13:48:06 +0200

    OCR

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,12 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
-import { Filesystem, Directory, FilesystemDirectory } from '@capacitor/filesystem';
+import { Filesystem, FilesystemDirectory } from '@capacitor/filesystem';
 import { FileOpener } from '@ionic-native/file-opener/ngx';
 import { Platform } from '@ionic/angular';
 import { ImageCroppedEvent } from 'ngx-image-cropper';
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
+import * as Tesseract from 'tesseract.js';
+import { createWorker } from 'tesseract.js';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
@@ -21,21 +23,56 @@ export class HomePage implements OnInit{
   photoPreview = null;
   logoData = null;
   croppedImage = null;
+  worker: Tesseract.Worker;
+  workerReady = false;
+  image = 'assets/ocrTestImage1.png';
+  ocrResult = '';
+  captureProgress = 0;
+  photoForOCR = null;
 
   constructor(
     private fb: FormBuilder,
     private plt: Platform,
     private http: HttpClient,
-    private fileOpener: FileOpener) {}
+    private fileOpener: FileOpener) {
+      this.loadWorker();
+    }
 
   ngOnInit() {
       this.myForm = this.fb.group({
         showLogo: true,
-        from: 'Simon',
-        to: 'Max',
-        text: 'TEST'
+        from: 'Michał',
+        to: 'Mateusz',
+        text: 'Add text...',
+        pdfFileName: 'Add file name...',
       });
       this.loadLocalAssetToBase64();
+  }
+
+  async loadWorker() {
+    this.worker = createWorker({
+      logger: progress => {
+        console.log(progress);
+        if (progress.status == 'recognizing text') {
+          this.captureProgress = parseInt('' + progress.progress * 100);
+        }
+      }
+    });
+    await this.worker.load();
+    await this.worker.loadLanguage('eng');
+    await this.worker.initialize('eng');
+    console.log('Worker loading has finnished');
+    this.workerReady = true;
+  }
+
+  async recognizeImage() {
+    try {
+      const result = await this.worker.recognize(this.photoForOCR);
+      console.log(result);
+      this.ocrResult = result.data.text;
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   loadLocalAssetToBase64() {
@@ -49,15 +86,34 @@ export class HomePage implements OnInit{
   }
 
   async takePicture() {
-    const image = await Camera.getPhoto({
-      quality: 100,
-      allowEditing: false,
-      resultType: CameraResultType.Base64,
-      source: CameraSource.Camera
-    });
-    console.log('image');
+    try {
+      const image = await Camera.getPhoto({
+        quality: 100,
+        allowEditing: false,
+        resultType: CameraResultType.Base64,
+        source: CameraSource.Camera
+      });
+      console.log('image');
+  
+      this.photoPreview = `data:image/jpeg;base64,${image.base64String}`;
+    } catch (err) {
+       console.error(err);
+    }
+  }
 
-    this.photoPreview = `data:image/jpeg;base64,${image.base64String}`;
+  async takePictureForOCR() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 100,
+        allowEditing: false,
+        resultType: CameraResultType.Base64,
+        source: CameraSource.Camera
+      });
+  
+      this.photoForOCR = `data:image/jpeg;base64,${image.base64String}`;
+    } catch (err) {
+       console.error(err);
+    }
   }
 
   imageCropped(event: ImageCroppedEvent) {
@@ -114,6 +170,7 @@ export class HomePage implements OnInit{
         },
         image,
         { text: formValue.text, margin: [0, 20, 0, 0] },
+        { text: this.ocrResult, margin: [0, 20, 0, 0] },
       ],
       styles: {
         header: {
@@ -135,7 +192,7 @@ export class HomePage implements OnInit{
     if (this.plt.is('cordova')) {
       this.pdfObj.getBase64(async (data) => {
         try {
-          let path = `pdf/myletter_${Date.now()}.pdf`;
+          let path = `pdf/${this.myForm.value.pdfFileName ?this.myForm.value.pdfFileName : 'myfile'}_${Date.now()}.pdf`;
 
           const result = await Filesystem.writeFile({
             path,
